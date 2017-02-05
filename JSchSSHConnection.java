069d79b8c984db1443a5a49c0e9875f56b25655d
Ram Paramhansa <ram.paramhansa@outlook.com>
2017-02-06 01:21:16 +0530

    first commit

---
+++ JSchSSHConnection.java
@@ -0,0 +1,62 @@
+package JSCHTest;
+
+import java.io.InputStream;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+
+public class JSchSSHConnection {
+
+	/**
+	 * JSch Example Tutorial
+	 * Java SSH Connection Program
+	 */
+	public static void main(String[] args) {
+	    String host="54.186.76.66";
+	    String user="user";
+	    String password="Chand@123";
+	    String command1="pwd";
+	    int port=22; 
+	    try{
+	    	
+	    	java.util.Properties config = new java.util.Properties(); 
+	    	config.put("StrictHostKeyChecking", "no");
+	    	JSch jsch = new JSch();
+	    	Session session=jsch.getSession(user, host, port);
+	    	session.setPassword(password);
+	    	session.setConfig(config);
+	    	session.connect();
+	    	System.out.println("Connected");
+	    	
+	    	Channel channel=session.openChannel("exec");
+	        ((ChannelExec)channel).setCommand(command1);
+	        channel.setInputStream(null);
+	        ((ChannelExec)channel).setErrStream(System.err);
+	        
+	        InputStream in=channel.getInputStream();
+	        channel.connect();
+	        byte[] tmp=new byte[1024];
+	        while(true){
+	          while(in.available()>0){
+	            int i=in.read(tmp, 0, 1024);
+	            if(i<0)break;
+	            System.out.print(new String(tmp, 0, i));
+	          }
+	          if(channel.isClosed()){
+	            System.out.println("exit-status: "+channel.getExitStatus());
+	            break;
+	          }
+	          try{Thread.sleep(1000);}catch(Exception ee){}
+	        }
+	        channel.disconnect();
+	        session.disconnect();
+	        System.out.println("DONE");
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+
+	}
+
+}
